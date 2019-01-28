cd63944fe4e1f37c2016bf5ce56d68b796ab404b
Juan <juan@daisho.group>
2019-01-28 10:23:24 +0100

    Removed commented code.

--- pkg/conductor/monitor/manager.go
+++ pkg/conductor/monitor/manager.go
@@ -64,11 +64,9 @@ func(m *Manager) UpdateFragmentStatus(request *pbConductor.DeploymentFragmentUpd
     var newStatus *pbApplication.UpdateAppStatusRequest
     newStatus = nil
 
-    //failedDeployment := false
 
     if entities.DeploymentStatusToGRPC[request.Status] == entities.FRAGMENT_DONE {
         log.Info().Str("fragmentId", request.FragmentId).Msgf("deployment fragment was done")
-        //m.pendingPlans.RemoveFragment(request.FragmentId)
         newStatus = &pbApplication.UpdateAppStatusRequest{
             OrganizationId: request.OrganizationId,
             AppInstanceId: request.AppInstanceId,
@@ -88,24 +86,8 @@ func(m *Manager) UpdateFragmentStatus(request *pbConductor.DeploymentFragmentUpd
     if entities.DeploymentStatusToGRPC[request.Status] == entities.FRAGMENT_ERROR {
         log.Info().Str("deploymentId", request.DeploymentId).Msg("deployment fragment failed")
         newStatus = m.processFailedFragment(request)
-        //failedDeployment = true
     }
 
-
-    // If no more fragments are pending... we stop monitoring the deployment plan
-    /*
-    if !failedDeployment && !m.pendingPlans.PlanHasPendingFragments(request.DeploymentId) {
-        log.Info().Str("deploymentId", request.DeploymentId).Msg("deployment plan was done")
-        // time to delete this plan
-        // m.pendingPlans.RemovePendingPlan(request.DeploymentId)
-        // update the application status in the system model
-        newStatus = &pbApplication.UpdateAppStatusRequest{
-            OrganizationId: request.OrganizationId,
-            AppInstanceId: request.AppInstanceId,
-            Status: pbApplication.ApplicationStatus_RUNNING,
-        }
-    }
-    */
 
     if newStatus != nil {
         _, err := m.AppClient.UpdateAppStatus(context.Background(), newStatus)
