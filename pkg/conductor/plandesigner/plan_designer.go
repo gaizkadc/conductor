d9c66618af2287f7434990244f325f655f0e123f
Marcos Garcia <mgarcia@nalej.com>
2019-10-02 11:18:16 +0200

    NP-2051 Add outbound variables to fragment deployment instructions

--- pkg/conductor/plandesigner/plan_designer.go
+++ pkg/conductor/plandesigner/plan_designer.go
@@ -6,40 +6,40 @@
 package plandesigner
 
 import (
-    "github.com/nalej/conductor/internal/entities"
-    "fmt"
-    "strings"
-    "time"
+	"fmt"
+	"github.com/nalej/conductor/internal/entities"
+	"github.com/nalej/conductor/pkg/utils"
+	"strings"
+	"time"
 )
 
 // Basic interface to be follow by any plan designer.
 
 type PlanDesigner interface {
-
-    // For any set of requirements an a given score elaborate a deployment plan. If a list of group ids is set
-    // only these groups are chosen otherwise, all the groups from the descriptor are used.
-    //  params:
-    //   app application instance
-    //   score obtained by musicians
-    //   request deployment request for this plan
-    //   groupIds list of group ids to select from the descriptor
-    //  return:
-    //   A collection of deployment plans each one designed to run in a different cluster.
-    DesignPlan(app entities.AppInstance,
-        score entities.DeploymentScore, request entities.DeploymentRequest, groupIds []string,
-        deployedGroups map[string][]string) (*entities.DeploymentPlan, error)
-
+	// For any set of requirements an a given score elaborate a deployment plan. If a list of group ids is set
+	// only these groups are chosen otherwise, all the groups from the descriptor are used.
+	//  params:
+	//   app application instance
+	//   score obtained by musicians
+	//   request deployment request for this plan
+	//   groupIds list of group ids to select from the descriptor
+	//  return:
+	//   A collection of deployment plans each one designed to run in a different cluster.
+	DesignPlan(app entities.AppInstance,
+		score entities.DeploymentScore, request entities.DeploymentRequest, groupIds []string,
+		deployedGroups map[string][]string) (*entities.DeploymentPlan, error)
 }
 
 const (
-    // Definition of the prefix to be used when defining any Nalej service variable
-    NalejVariablePrefix = "NALEJ_SERV_%s"
-    // Nalej service suffix
-    NalejServiceSuffix = "service.nalej"
-    // Timeout for GRPC operations
-    PlanDesignerGRPCTimeout = 5 * time.Second
+	// Definition of the prefix to be used when defining any Nalej service variable
+	NalejVariablePrefix = "NALEJ_SERV_%s"
+	// Definition of the prefix to be used when defining any Nalej outbound variable
+	NalejVariableOutboundPrefix = "NALEJ_OUTBOUND_%s"
+	// Nalej service suffix
+	NalejServiceSuffix = "service.nalej"
+	// Timeout for GRPC operations
+	PlanDesignerGRPCTimeout = 5 * time.Second
 )
-
 
 // Generate the tuple key and value for a nalej service to be represented.
 // params:
@@ -49,17 +49,23 @@ const (
 // return:
 //  variable name, variable value
 func GetDeploymentVariableForService(serviceName string, appInstanceId string, organizationId string) (string, string) {
-    key := fmt.Sprintf(NalejVariablePrefix,strings.ToUpper(serviceName))
-    value := fmt.Sprintf("%s-%s-%s.%s", formatName(serviceName), organizationId[0:10],
-        appInstanceId[0:10], NalejServiceSuffix)
-    return key,value
+	key := fmt.Sprintf(NalejVariablePrefix, strings.ToUpper(serviceName))
+	value := fmt.Sprintf("%s-%s-%s.%s", formatName(serviceName), organizationId[0:10],
+		appInstanceId[0:10], NalejServiceSuffix)
+	return key, value
 }
 
+func GetDeploymentVariableForOutbound(serviceName string, appInstanceId string, organizationId string) (string, string) {
+	key := fmt.Sprintf(NalejVariableOutboundPrefix, strings.ToUpper(serviceName))
+	// TODO ensure that this string is the one that must be used
+	value := utils.GetVSAName(serviceName, organizationId, appInstanceId)
+	return key, value
+}
 
 // Format a string removing white spaces and going lowercase
 func formatName(name string) string {
-    aux := strings.ToLower(name)
-    // replace any space
-    aux = strings.Replace(aux, " ", "", -1)
-    return aux
+	aux := strings.ToLower(name)
+	// replace any space
+	aux = strings.Replace(aux, " ", "", -1)
+	return aux
 }
